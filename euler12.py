6588e8aa69cc9882e970855e6aa462aa0e260403
Nathan Rapport <n.rapport@gmail.com>
2012-07-09 20:44:24 -0500

    add 05

--- euler12.py
+++ euler12.py
@@ -5,11 +5,11 @@ n=1
 trinum=0
 counter=0
 while counter<=250:
-	counter = 0
-	trinum = trinum + n
-	n=n+1
+	counter=0
+	trinum+=n
+	n+=1
 	for i in range(1,int(math.sqrt(trinum))):
 		if trinum%i==0:
-			counter = counter+1
+			counter+=1
 print trinum
 print 'run time =', time.time() - tstart
